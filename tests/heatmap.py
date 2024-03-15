1ffedaca0966c8a417f9e2e5ae7dacbf011f48e4
Luis El Srouji <lzelsrouji@ucdavis.edu>
2024-03-15 15:14:13 -0700

    updated heatmap network

--- tests/heatmap.py
+++ tests/heatmap.py
@@ -1,13 +1,9 @@
-from copy import deepcopy
-import os
-from io import BytesIO
-from math import ceil, floor
-
 from vivilux import *
-import vivilux as vl
-from vivilux.learningRules import CHL, GeneRec
-from vivilux.optimizers import Momentum
-from vivilux.visualize import Heatmap
+from vivilux.nets import Net, layerConfig_std
+from vivilux.layers import Layer
+from vivilux.meshes import Mesh
+from vivilux.metrics import RMSE, ThrMSE, ThrSSE
+from vivilux.visualize import Record, Heatmap
 
 import numpy as np
 from sklearn import datasets
@@ -16,49 +12,78 @@ import networkx as nx
 import imageio
 np.random.seed(seed=0)
 
+from copy import deepcopy
 
-numEpochs = 100
-numSamples = 1
+numEpochs = 50
+inputSize = 25
+hiddenSize = 49
+outputSize = 25
+patternSize = 6
+numSamples = 25
+
+#define input and output data (must be one-hot encoded)
+inputs = np.zeros((numSamples, inputSize))
+inputs[:,:patternSize] = 1
+inputs = np.apply_along_axis(np.random.permutation, axis=1, arr=inputs) 
+targets = np.zeros((numSamples, outputSize))
+targets[:,:patternSize] = 1
+targets = np.apply_along_axis(np.random.permutation, axis=1, arr=targets)
+
+leabraRunConfig = {
+    "DELTA_TIME": 0.001,
+    "metrics": {
+        "AvgSSE": ThrMSE,
+        "SSE": ThrSSE,
+        "RMSE": RMSE
+    },
+    "outputLayers": {
+        "target": -1,
+    },
+    "Learn": ["minus", "plus"],
+    "Infer": ["minus"],
+}
+
+leabraNet = Net(name = "LEABRA_NET",
+                runConfig=leabraRunConfig) # Default Leabra net
+
+# Add layers
+layerList = [Layer(inputSize, isInput=True, name="Input"),
+             Layer(hiddenSize, name="Hidden1"),
+             Layer(hiddenSize, name="Hidden2"),
+             Layer(outputSize, isTarget=True, name="Output")]
+leabraNet.AddLayers(layerList[:-1])
+outputConfig = deepcopy(layerConfig_std)
+outputConfig["FFFBparams"]["Gi"] = 1.4
+leabraNet.AddLayer(layerList[-1], layerConfig=outputConfig)
+
+# Add feedforward connections
+ffMeshes = leabraNet.AddConnections(layerList[:-1], layerList[1:])
+# Add feedback connections
+fbMeshConfig = {"meshType": Mesh,
+                "meshArgs": {"AbsScale": 1,
+                             "RelScale": 0.2},
+                }
+fbMeshes = leabraNet.AddConnections(layerList[1:], layerList[:-1],
+                                    meshConfig=fbMeshConfig)
 
 
-#define input and output data (must be normalized and positive-valued)
-iris = datasets.load_iris()
-inputs = iris.data
-maxMagnitude = np.max(np.sqrt(np.sum(np.square(inputs), axis=1)))
-inputs = inputs/maxMagnitude # bound on (0,1]
-targets = np.zeros((len(inputs),4))
-targets[np.arange(len(inputs)), iris.target] = 1
-#shuffle both arrays in the same manner
-shuffle = np.random.permutation(len(inputs))
-inputs, targets = inputs[shuffle][:numSamples], targets[shuffle][:numSamples]
 
-optArgs = {"lr": 0.015}
+heatmap = Heatmap(leabraNet, numEpochs, numSamples)
 
-netMixed = RecurNet([
-    Layer(4, isInput=True),
-    Layer(4, learningRule=CHL),
-    Layer(4, learningRule=CHL),
-    Layer(4, learningRule=CHL)
-    ],
-    AbsMesh,
-    monitoring=True, defMonitor = vl.visualize.Record,
-    optArgs=optArgs,
-    name = "NET_Mixed")
-
-heatmap = Heatmap(netMixed, numEpochs, numSamples)
-
-resultMixed = netMixed.Learn(inputs, targets,
-                             numEpochs=numEpochs,
-                             shuffle=False,
-                             reset=False)
+result = leabraNet.Learn(input=inputs, target=targets,
+                         numEpochs=numEpochs,
+                         reset=False,
+                         shuffle=False,
+                         EvaluateFirst=False,
+                         )
 
 heatmap.animate("single-training-Run")
 
 # Plot RMSE over time
 plt.figure()
-plt.plot(resultMixed, label="Mixed")
-baseline = np.mean([RMSE(entry, targets) for entry in np.random.uniform(size=(2000,numSamples,4))])
-plt.axhline(y=baseline, color="b", linestyle="--", label="baseline guessing")
+plt.plot(result, label="Mixed")
+# baseline = np.mean([RMSE(entry, targets) for entry in np.random.uniform(size=(2000,numSamples,4))])
+# plt.axhline(y=baseline, color="b", linestyle="--", label="baseline guessing")
 plt.title("Training Demo on Single Sample")
 plt.ylabel("Accuracy")
 plt.xlabel("Epoch")
