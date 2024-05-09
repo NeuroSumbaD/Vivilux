9933c79cad294cd0085807c86086c559318b9bdb
Luis El Srouji <lzelsrouji@ucdavis.edu>
2024-05-09 13:49:54 -0700

    added fffb_sweep result and plotting script

---
+++ tests/fffb_sweep_plot.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+import pathlib
+from os import path
+
+directory = pathlib.Path(__file__).parent.resolve()
+df = pd.read_csv(path.join(directory,"fffb_sweep_results.csv"))
+
+df = df[df["Gi"] == 1.3]
+# df = df[df["FF"] == 1]
+# df = df[df["FB"] == 1]
+# df = df[df["FBTau"] == 1.4]
+df = df[df["MaxVsAvg"] == 0]
+df = df[df["FF0"] == 0.1]
+
+plt.figure()
+plt.scatter(df["inL1"], df["outL1"])
+plt.title("Input Norm vs Output Norm")
+plt.xlabel("inL1")
+plt.ylabel("outL1")
+plt.show()
