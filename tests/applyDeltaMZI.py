b1f9c5481458fb7f82504fc66341cf05ae57f95b
Luis El Srouji <lzelsrouji@ucdavis.edu>
2024-05-09 15:29:14 -0700

    updated applyDelta to include random unitary matrices

--- tests/applyDeltaMZI.py
+++ tests/applyDeltaMZI.py
@@ -22,6 +22,7 @@ mzi = MZImesh(matrixSize, dummyLayer)
 rows = list(np.eye(matrixSize)) # list of rows for permutation matrix
 records = [] # to store traces of magnitude for each permutation matrix
 plt.figure()
+print("--------STARTING TEST: All Permutations--------")
 for perm in permutations(rows):
     matrix = np.array(perm)
     initMatrix = mzi.get()/mzi.Gscale
@@ -40,6 +41,43 @@ for perm in permutations(rows):
     plt.plot(mzi.record[mzi.record>=0])
 
 plt.title("Implementing Permutation Matrices")
+plt.ylabel("magnitude of difference vector")
+plt.xlabel("LAMM iteration")
+
+# Random Unitary
+dummyLayer = Layer(matrixSize, isInput=True, name="Input")
+dummyNet = Net(name = "LEABRA_NET")
+dummyNet.AddLayer(dummyLayer)
+mzi = MZImesh(matrixSize, dummyLayer,
+              numDirections=8,
+              numSteps=500,
+              rtol=1e-2,
+            #   updateMagnitude=0.05,
+              )
+plt.figure()
+print("--------STARTING TEST: Random Unitary--------")
+records = [] # to store traces of magnitude for each matrix
+numUnits = int(matrixSize*(matrixSize-1)/2)
+for index in range(50):
+    initMatrix = mzi.get()/mzi.Gscale
+    randMatrix = np.square(np.abs(psToRect(np.random.rand(numUnits,2)*2*np.pi,
+                                           matrixSize)))
+    initDelta = randMatrix - initMatrix
+    magnitude, numSteps = mzi.set(matrix)
+    print("Attempting to implement:")
+    print(matrix)
+    print("Initial matrix")
+    print(np.round(initMatrix, 2))
+    print("Resulting matrix:")
+    print(np.round(mzi.matrix, 2))
+    print("Initial magnitude:", np.sum(np.square(initDelta)))
+    print("Final delta magnitude:", magnitude)
+    print(f"Took {numSteps} steps.")
+    records.append(mzi.record)
+    plt.plot(mzi.record[mzi.record>=0])
+
+    magnitude, numSteps = mzi.set(randMatrix)
+plt.title("Implementing Random Unitary Matrices")
 plt.ylabel("magnitude of difference vector")
 plt.xlabel("LAMM iteration")
 plt.show()
