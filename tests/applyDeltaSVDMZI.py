551a87972fca13cec9885fe1c6aae643a5f4633f
Luis El Srouji <lzelsrouji@ucdavis.edu>
2024-05-30 10:28:34 -0700

    added additional sparse convergence trace

--- tests/applyDeltaSVDMZI.py
+++ tests/applyDeltaSVDMZI.py
@@ -18,18 +18,18 @@ numIterations = 5
 dummyLayer = Layer(matrixSize, isInput=True, name="Input")
 dummyNet = Net(name = "LEABRA_NET")
 dummyNet.AddLayer(dummyLayer)
-mzi = SVDMZI(matrixSize, dummyLayer,
-             numDirections=16,
-             numSteps=500,
-             rtol=5e-2,
-             updateMagnitude=1e-4,
-             )
 
 rows = list(np.eye(matrixSize)) # list of rows for permutation matrix
 records = [] # to store traces of magnitude for each permutation matrix
 plt.figure()
 print("--------STARTING TEST: Small parameter deviations--------")
 for index in range(numIterations):
+    mzi = SVDMZI(matrixSize, dummyLayer,
+              numDirections=16,
+              numSteps=500,
+              rtol=1e-2,
+              updateMagnitude=1e-4,
+              )
     matrix = mzi.getFromParams([0.2*np.random.rand(*param.shape)-0.1 + param for param in mzi.getParams()])
     initMatrix = mzi.get()/mzi.Gscale
     initDelta = matrix - initMatrix
@@ -54,17 +54,17 @@ plt.xlabel("LAMM iteration")
 dummyLayer = Layer(matrixSize, isInput=True, name="Input")
 dummyNet = Net(name = "LEABRA_NET")
 dummyNet.AddLayer(dummyLayer)
-mzi = SVDMZI(matrixSize, dummyLayer,
-              numDirections=16,
-              numSteps=500,
-              rtol=1e-2,
-              updateMagnitude=1e-3,
-              )
 plt.figure()
 print("--------STARTING TEST: Random Matrix on [0,1)--------")
 records = [] # to store traces of magnitude for each matrix
 numUnits = int(matrixSize*(matrixSize-1)/2)
 for index in range(numIterations):
+    mzi = SVDMZI(matrixSize, dummyLayer,
+                numDirections=16,
+                numSteps=500,
+                rtol=1e-2,
+                updateMagnitude=1e-3,
+                )
     initMatrix = mzi.get()/mzi.Gscale
     randMatrix = np.random.rand(4,4)
     initDelta = randMatrix - initMatrix
@@ -85,4 +85,47 @@ for index in range(numIterations):
 plt.title("Implementing Random Matrices")
 plt.ylabel("magnitude of difference vector")
 plt.xlabel("LAMM iteration")
+
+plt.figure()
+print("--------STARTING TEST: Sparse Delta Convergence [0,1)--------")
+records = [] # to store traces of magnitude for each matrix
+numUnits = int(matrixSize*(matrixSize-1)/2)
+for index in range(numIterations):
+    mzi = SVDMZI(matrixSize, dummyLayer,
+              numDirections=16,
+              numSteps=500,
+              rtol=1e-2,
+              updateMagnitude=1e-3,
+              )
+    initMatrix = mzi.get()/mzi.Gscale
+
+    numSparseElements = 5 # out of 16
+    randMatrix = np.copy(initMatrix)
+    randCoeff = 5e-4*np.random.rand(1)
+    indices = np.random.permutation(16)[:numSparseElements]
+    mask = np.zeros(16)
+    mask[indices] = 1
+    mask = mask.reshape(4,4).astype("bool")
+    randMatrix[mask] = 2 * (np.random.rand(numSparseElements) - 0.5)
+    randMatrix[mask] *= randCoeff
+
+    initDelta = randMatrix - initMatrix
+    magnitude, numSteps = mzi.set(matrix)
+    print("Attempting to implement:")
+    print(np.round(matrix,6))
+    print("Initial matrix")
+    print(np.round(initMatrix, 6))
+    print("Resulting matrix:")
+    print(np.round(mzi.matrix, 6))
+    print("Initial magnitude:", np.sum(np.square(initDelta)))
+    print("Final delta magnitude:", magnitude)
+    print(f"Took {numSteps} steps.")
+    records.append(mzi.record)
+    plt.plot(mzi.record[mzi.record>=0])
+
+    magnitude, numSteps = mzi.set(randMatrix)
+plt.title("Implementing Sparse Deltas")
+plt.ylabel("magnitude of difference vector")
+plt.xlabel("LAMM iteration")
+
 plt.show()
