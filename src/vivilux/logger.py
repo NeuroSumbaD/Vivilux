b31d640946f6c74f9a17b4a74a5f6541a4fa54f7
Luis El Srouji <lzelsrouji@ucdavis.edu>
2025-06-05 11:11:00 -0700

    fixed logger path

--- src/vivilux/logger.py
+++ src/vivilux/logger.py
@@ -9,15 +9,22 @@ import __main__
 
 # Set up logging
 def setup_logger():
-    filename = os.path.basename(__main__.__file__)
+    main_file = os.path.abspath(__main__.__file__)
+    main_dir = os.path.dirname(main_file)
+    logs_dir = os.path.join(main_dir, "logs")
+    os.makedirs(logs_dir, exist_ok=True)  # Ensure the logs directory exists
+
+    filename = os.path.basename(main_file)
     filename = filename.replace('.py', '.log')
+    log_path = os.path.join(logs_dir, filename)
+
     logger = logging.getLogger(filename)
     logger.setLevel(logging.DEBUG)
     
     # Check if handler already exists to avoid duplicates
     if not logger.handlers:
         # Create file handler and set formatter
-        file_handler = logging.FileHandler(filename)
+        file_handler = logging.FileHandler(log_path)
         formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
         file_handler.setFormatter(formatter)
         logger.addHandler(file_handler)
