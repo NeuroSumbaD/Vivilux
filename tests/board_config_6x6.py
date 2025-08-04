03ba5c61ce75fba598ee4800f6a57f14e6b6c84f
Luis El Srouji <lzelsrouji@ucdavis.edu>
2025-08-04 16:01:22 -0700

    updated the board configuration after moving some wires and made a new script for the four-input array

--- tests/board_config_6x6.py
+++ tests/board_config_6x6.py
@@ -8,52 +8,68 @@ import vivilux.hardware.mcc as mcc
 import vivilux.hardware.nidaq as ni
 
 ni_boards =[
-    ni.USB_6210("NI", 33703915,
-        ni.AIPIN("PD_1_0", 0),
-        ni.AIPIN("PD_1_1", 1),
-        ni.AIPIN("PD_1_2", 2),
-        ni.AIPIN("PD_1_3", 3),
-        ni.AIPIN("PD_1_4", 4),
-        ni.AIPIN("PD_1_5", 5),
-        ni.AIPIN("PD_2_0", 6),
-        ni.AIPIN("PD_2_1", 7),
-        ni.AIPIN("PD_2_2", 8),
-        ni.AIPIN("PD_2_3", 9),
+    ni.USB_6210("NI", 33703915, # NOTE: ni board is currently the only one with the necessary range for single-ended readout
+        ni.AIPIN("PD_1_0", 0), # DON'T CHANGE
+        # ni.AIPIN("PD_1_1", 1), #  MOVED
+        # ni.AIPIN("PD_1_2", 2), #  MOVED
+        # ni.AIPIN("PD_1_3", 3), #  MOVED
+        # ni.AIPIN("PD_1_4", 4), #  MOVED
+        ni.AIPIN("PD_1_5", 5), # DON'T CHANGE
+        ni.AIPIN("PD_2_0", 6), # DON'T CHANGE
+        # ni.AIPIN("PD_2_1", 7), #  MOVED
+        # ni.AIPIN("PD_2_2", 8), #  MOVED
+        # ni.AIPIN("PD_2_3", 9), #  MOVED
         ni.AIPIN("PD_2_4", 10),
-        ni.AIPIN("PD_2_5", 11),
-        ni.AIPIN("PD_3_0", 12),
+        ni.AIPIN("PD_2_5", 11), # DON'T CHANGE
+        ni.AIPIN("PD_3_0", 12), # DON'T CHANGE
         ni.AIPIN("PD_3_1", 13),
         ni.AIPIN("PD_3_2", 14),
         ni.AIPIN("PD_3_3", 15),
+        
+        ni.AIPIN("PD_4_0", 1), # From TOP mcc.AIPIN("PD_4_0", 2)
+        ni.AIPIN("PD_5_0", 2), # From MIDDLE mcc.AIPIN("PD_5_0", 0)
+        ni.AIPIN("PD_6_0", 3), # From MIDDLE mcc.AIPIN("PD_6_0", 6)
+        
+        ni.AIPIN("PD_3_5", 4), # From TOP mcc.AIPIN("PD_3_5", 1)
+        ni.AIPIN("PD_4_5", 7), # From TOP mcc.AIPIN("PD_4_5", 7)
+        ni.AIPIN("PD_5_5", 8), # From MIDDLE mcc.AIPIN("PD_5_5", 5)
+        ni.AIPIN("PD_6_5", 9), # From BOTTOM mcc.AIPIN("PD_6_5", 3)
     ),
 ]
 
 mcc_boards = [
-    mcc.USB_1208FS_PLUS("ADC 1", "21E81FE",
+    mcc.USB_1208FS_PLUS("ADC 1", "21E81FE", #BOTTOM
         mcc.AIPIN("PD_3_4", 0), # Pin 1
-        mcc.AIPIN("PD_3_5", 1), # Pin 2
-        mcc.AIPIN("PD_4_0", 2), # Pin 4
+        # mcc.AIPIN("PD_3_5", 1), # Pin 2 #  MOVED
+        mcc.AIPIN("PD_1_4", 1), # NEW LOCATION
+        # mcc.AIPIN("PD_4_0", 2), # Pin 4 # MOVED
+        mcc.AIPIN("PD_1_1", 2), # NEW LOCATION
         mcc.AIPIN("PD_4_1", 3), # Pin 5
         mcc.AIPIN("PD_4_2", 4), # Pin 7
         mcc.AIPIN("PD_4_3", 5), # Pin 8
         mcc.AIPIN("PD_4_4", 6), # Pin 10
-        mcc.AIPIN("PD_4_5", 7), # Pin 11
+        # mcc.AIPIN("PD_4_5", 7), # Pin 11 #  MOVED
+        mcc.AIPIN("PD_2_1", 7), # NEW LOCATION
     ),
-    mcc.USB_1208FS_PLUS("ADC 2", "21E81BD",
-        mcc.AIPIN("PD_5_0", 0), # Pin 1
+    mcc.USB_1208FS_PLUS("ADC 2", "21E81BD", # MIDDLE
+        # mcc.AIPIN("PD_5_0", 0), # Pin 1 # MOVED
+        mcc.AIPIN("PD_1_2", 0), # NEW LOCATION
         mcc.AIPIN("PD_5_1", 1), # Pin 2
         mcc.AIPIN("PD_5_2", 2), # Pin 4
         mcc.AIPIN("PD_5_3", 3), # Pin 5
         mcc.AIPIN("PD_5_4", 4), # Pin 7
-        mcc.AIPIN("PD_5_5", 5), # Pin 8
-        mcc.AIPIN("PD_6_0", 6), # Pin 10
+        # mcc.AIPIN("PD_5_5", 5), # Pin 8 #  MOVED
+        mcc.AIPIN("PD_2_2", 5), # NEW LOCATION
+        # mcc.AIPIN("PD_6_0", 6), # Pin 10 # MOVED
+        mcc.AIPIN("PD_1_3", 6), # NEW LOCATION
         mcc.AIPIN("PD_6_1", 7), # Pin 11
     ),
-    mcc.USB_1208FS_PLUS("ADC 3", "21E8232",
+    mcc.USB_1208FS_PLUS("ADC 3", "21E8232", # TOP
         mcc.AIPIN("PD_6_2", 0), # Pin 1
         mcc.AIPIN("PD_6_3", 1), # Pin 2
         mcc.AIPIN("PD_6_4", 2), # Pin 4
-        mcc.AIPIN("PD_6_5", 3), # Pin 5
+        # mcc.AIPIN("PD_6_5", 3), # Pin 5 #  MOVED
+        mcc.AIPIN("PD_2_3", 3), # NEW LOCATION
     ),
     mcc.USB_3114("DAC", "216B19F",
         mcc.AOPIN("1_1_i", 1),
@@ -75,7 +91,7 @@ mcc_boards = [
         mcc.AOPIN("laser_1", 0),
         mcc.AOPIN("laser_2", 2),
         mcc.AOPIN("laser_3", 4),
-        # mcc.AOPIN("laser_4", 6),
+        mcc.AOPIN("laser_4", 6),
         # mcc.AOPIN("laser_5", 1),
         # mcc.AOPIN("laser_6", 3),
     ),
