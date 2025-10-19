fixed multiple math errors in matrix column norm, derivative measurements; new best calibration run for crirel

src/vivilux/hardware/arbitrary_mzi.py
@@ -6,7 +6,7 @@
 '''
 
 from time import sleep
-from typing import Optional
+from typing import Optional, Callable
 
 import numpy as np
 
@@ -17,6 +17,69 @@ from vivilux.hardware.lasers import LaserArray #, InputGenerator
 from vivilux.hardware.detectors import DetectorArray
 from vivilux.hardware import daq
 from vivilux.logger import log
+
+type StepGenerator = Callable[[int], np.ndarray] # Type alias for step vector generator
+
+def gen_from_uniform(shape: tuple[int, ...]) -> StepGenerator:
+    '''Generates step vectors from a uniform distribution between -0.5 and 0.5.
+    '''
+    def _generator(num_vectors = 1) -> np.ndarray:
+        return (np.random.rand(num_vectors, *shape)-0.5)
+    return _generator
+
+def gen_from_sparse_permutation(shape: tuple[int, ...], numHot: int) -> StepGenerator:
+    '''Generates step vectors with a fixed number of hot (1.0) entries at
+        random positions.
+        
+        NOTE: Because numHot is an additional parameter, this generator must be
+        used with `Partial` from the `functools` module to fix the numHot value.
+        e.g. `step_generator=partial(gen_from_sparse_permutation, numHot=3)`
+    '''
+    def _generator(num_vectors=1) -> np.ndarray:
+        arr = np.zeros((num_vectors, *shape))
+        for i in range(num_vectors):
+            hot_indices = np.random.choice(np.prod(shape), size=numHot, replace=False)
+            np.put(arr[i], hot_indices, 1.0)
+        return arr
+    return _generator
+
+def gen_from_one_hot(shape: tuple[int, ...]) -> StepGenerator:
+    '''Generates one-hot step vectors.
+    
+        NOTE: Using sparse permutation with numHot=1 would achieve the same
+        effect but may generate duplicate vectors.
+    '''
+    def _generator(num_vectors=1) -> np.ndarray:
+        arr = np.eye(np.prod(shape))[np.random.permutation(np.prod(shape))]
+        arr = arr[:num_vectors].reshape((num_vectors, *shape))
+        return arr
+    return _generator
+
+def least_squares_solver(X: np.ndarray, V: np.ndarray,
+                         deltaFlat: np.ndarray,
+                         ) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
+    # minimize least squares difference to deltas
+    a = None
+    for iteration in range(X.shape[1]):
+        xtx = X.T @ X
+        rank = np.linalg.matrix_rank(xtx)
+        if rank == len(xtx): # matrix will have an inverse
+            a = np.linalg.inv(xtx) @ X.T @ deltaFlat
+            break
+        else: # direction vectors cary redundant information use one less
+            X = X[:,:-1]
+            V = V[:,:-1]
+            continue
+    if a is None:
+        raise RuntimeError("ERROR: Could not compute least squares solution")
+    return a, X, V
+
+def least_squares_solver_v2(X: np.ndarray, V: np.ndarray,
+                            deltaFlat: np.ndarray,
+                            ) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
+    xtx = X.T @ X
+    a = np.linalg.pinv(xtx) @ X.T @ deltaFlat
+    return a, X, V
 
 class HardMZI_v3(MZImesh):
     '''Updated version of HardMZI that uses the daq modules for generating and
@@ -40,6 +103,7 @@ class HardMZI_v3(MZImesh):
                  check_stop: int = 5, # exits calibration loop after this many iterations with no improvement
                  initialize: bool = True, # whether to calculate the initial MZI matrix
                  use_norm: bool = True,  # whether to normalize the output columns
+                 step_generator: Callable[[tuple[int, ...]], StepGenerator] = gen_from_uniform, # step vector generator (used during matrix gradient calculation)
                  **kwargs):
         # TODO: add support for attachment to Leabra Net
         # Mesh.__init__(self, size, inLayer, **kwargs)
@@ -68,6 +132,7 @@ class HardMZI_v3(MZImesh):
             (self.psLimits[1]-self.psLimits[0])/2 + self.psLimits[0]
         self.powers = np.square(self.voltages) # power is proportional to square of voltage
 
+        self._gen_step_vector = step_generator(self.voltages.shape)
         
         self.resetDelta = np.zeros(self.shape)
 
@@ -126,8 +191,17 @@ class HardMZI_v3(MZImesh):
             self.netlist[self.psPins[index]].vout(volt)
         sleep(self.ps_delay)  # Allow time for the voltages to settle
 
-    def testParams(self, params: list[np.ndarray]):
-        '''Temporarily set the params'''
+    def testParams(self, params: list[np.ndarray], measure: bool = True) -> np.ndarray | None:
+        '''Temporarily set the params.
+        
+        Parameters
+        ----------
+        params : list[np.ndarray]
+            List containing a single numpy array of phase shifter powers.
+        measure : bool, optional
+            Whether to measure the matrix after setting the params. Default is True.
+        
+        '''
         params = self.clipParams(params)
         powers = params[0].flatten()  # Flatten the array to 1D
         voltages = np.sqrt(powers)  # Convert powers to voltages
@@ -140,6 +214,9 @@ class HardMZI_v3(MZImesh):
             self.netlist[self.psPins[index]].vout(volt)
         sleep(self.ps_delay)  # Allow time for the voltages to settle
 
+        if not measure:
+            return None
+        
         powerMatrix = np.zeros(self.shape) # for pass by reference
         powerMatrix = self.measureMatrix(powerMatrix)
         self.resetParams()
@@ -167,6 +244,10 @@ class HardMZI_v3(MZImesh):
             TODO: Improve this method to not use an external matrix.
         '''
         oneHots = np.eye(self.inputLaser.size) # create one-hot vectors for each channel
+        calculateNorms = False
+        if not hasattr(self, "norm_factors"):
+            self.norm_factors = np.zeros(self.shape)
+            calculateNorms = True
         for chan in range(self.inputLaser.size): # iterate over the input waveguides
             oneHot = oneHots[chan] # get the one-hot vector for this channel
             self.inputLaser.setNormalized(oneHot) # apply the one-hot vector
@@ -181,7 +262,9 @@ class HardMZI_v3(MZImesh):
                 log.warning(f"Warning: Zero norm on chan={chan}. Column readout:\n{columnReadout}")
             else:
                 if self.use_norm:
-                    column /= L1norm(column)
+                    if calculateNorms:
+                        self.norm_factors[:,chan] = L1norm(column)
+                    column /= self.norm_factors[:,chan]  # normalize the column
 
             powerMatrix[:,chan] = column
         
@@ -288,10 +371,9 @@ class HardMZI_v3(MZImesh):
             Returns derivativeMatrix, stepVector
         '''
         updateMagnitude = self.updateMagnitude
-        # TODO: Test normal distribution for better sampled step vectors
-        stepVector = (np.random.rand(*self.voltages.shape)-0.5) if stepVector is None else stepVector
-        # stepVector = (np.random.normal(size=self.voltages.shape)) if stepVector is None else stepVector
-        # stepVector = np.sign(np.random.rand(*voltages.shape)-0.5) if stepVector is None else stepVector
+        # TODO: Test various distributions for stepVector here
+        # stepVector = (np.random.rand(*self.voltages.shape)-0.5) if stepVector is None else stepVector
+        stepVector = self._gen_step_vector(1)[0] if stepVector is None else stepVector
         randMagnitude = np.sqrt(np.sum(np.square(stepVector)))
         stepVector = stepVector/randMagnitude
         stepVector = stepVector*updateMagnitude # update magnitude refers to the magnitude of the powers
@@ -301,21 +383,43 @@ class HardMZI_v3(MZImesh):
         derivativeMatrix = np.zeros(currMat.shape)
     
         # Forward step
-        plusVectors = self.powers + stepVector
+        plusVectors = self.powers.flatten() + stepVector.flatten()
         log.debug(f"Plus vector step: {plusVectors.flatten()}")
         # Backward step
-        minusVectors = self.powers - stepVector
+        minusVectors = self.powers.flatten() - stepVector.flatten()
         log.debug(f"Minus vector step: {minusVectors.flatten()}")
         
         # Calculate the plus and minus matrices
-        plusMatrix = self.testParams([plusVectors])
-        minusMatrix = self.testParams([minusVectors])
+        differenceMatrix = np.zeros(currMat.shape)
+        std_matrix = np.zeros(currMat.shape)
+        for col in range(currMat.shape[1]):
+            oneHot = np.zeros(self.inputLaser.size, dtype=int)
+            oneHot[col] = 1
+            self.inputLaser.setNormalized(oneHot)
+            self.testParams([plusVectors.reshape(-1,1)], measure=False)
+            plusReadout, plus_dev = self.outputDetectors.read_raw()
+            log.debug(f"Plus readout for column {col} (mV): {plusReadout*1e3} ± {plus_dev*1e3}")
 
-        differenceMatrix = plusMatrix-minusMatrix
-        derivativeMatrix = differenceMatrix/(2*updateMagnitude)
+            self.testParams([minusVectors.reshape(-1,1)], measure=False)
+            minusReadout, minus_dev = self.outputDetectors.read_raw()
+            log.debug(f"Minus readout for column {col} (mV): {minusReadout*1e3} ± {minus_dev*1e3}")
+
+            differenceMatrix[:,col] = plusReadout - minusReadout
+            std_matrix[:,col] = plus_dev + minus_dev # propagate uncertainty assuming independence
+            if self.use_norm:
+                differenceMatrix[:,col] /= self.norm_factors[:,col]
+                std_matrix[:,col] /= self.norm_factors[:,col]
+
+        # NOTE: The derivative is calculated as -difference/(2*updateMagnitude) because the
+        # measurement yields dV/dParam and we want dI/dParam, where I is proportional to V
+        # based on the photodetector response I = (offset-reading)/transimpedance, so an
+        # additional factor of transimpedance is also included (dI=-dV/transimpedance).
+        mask = np.abs(differenceMatrix) < std_matrix
+        differenceMatrix[mask] = 0.0  # zero out any differences that are less than the noise level
+        derivativeMatrix = -differenceMatrix/(2*updateMagnitude*self.outputDetectors.transimpedance)
         
 
-        return derivativeMatrix, stepVector/updateMagnitude
+        return derivativeMatrix, stepVector
 
 
     def getGradients(self,
@@ -332,12 +436,15 @@ class HardMZI_v3(MZImesh):
         X = np.zeros((deltaFlat.shape[0], numDirections))
         V = np.zeros((thetaFlat.shape[0], numDirections))
         
+        # Generate step vectors and check they are not identical
+        step_vectors = self._gen_step_vector(numDirections)       
+            
         # Calculate directional derivatives
         for i in range(numDirections):
             if verbose:
                 print(f"\tGetting derivative {i}")
             # TODO: Find some constraints for the correlation between the stepVectors
-            tempx, tempv = self.matrixGradient()
+            tempx, tempv = self.matrixGradient(stepVector=step_vectors[i].reshape(-1,1))
             tempv = np.concatenate([param.flatten() for param in tempv])
             X[:,i], V[:,i] = tempx[:m, :n].flatten(), tempv.flatten()
 
@@ -366,22 +473,27 @@ class HardMZI_v3(MZImesh):
             # currMat = self.get()/self.Gscale
             currMat = self.get()
             print(f"Step: {step}, magnitude delta = {magnitude(deltaFlat)}")  
-            X, V = self.getGradients(delta, newPs, numDirections, verbose)
-            # minimize least squares difference to deltas
-            for iteration in range(numDirections):
-                xtx = X.T @ X
-                rank = np.linalg.matrix_rank(xtx)
-                if rank == len(xtx): # matrix will have an inverse
-                    a = np.linalg.inv(xtx) @ X.T @ deltaFlat
-                    break
-                else: # direction vectors cary redundant information use one less
-                    X = X[:,:-1]
-                    V = V[:,:-1]
-                    continue
+            X_raw, V_raw = self.getGradients(delta, newPs, numDirections, verbose)
+            derivative_norms = np.linalg.norm(X_raw, axis=0)
+            # Drop any zero-norm directions
+            X_raw = X_raw[:, derivative_norms != 0]
+            V_raw = V_raw[:, derivative_norms != 0]
+            # Scale deltaFlat to reasonable step size based on derivative norms
+            deltaNorm = np.linalg.norm(deltaFlat)
+            scaledDelta = deltaFlat
+            if deltaNorm > np.sum(derivative_norms): # scale down if it is significantly larger than the total derivative norm
+                normRescale = np.sum(derivative_norms) / np.linalg.norm(deltaFlat)
+                scaledDelta *= normRescale
+            # scaledDelta *= np.minimum([deltaNorm, self.updateMagnitude]) / deltaNorm # Alternative rescaling
+            a, X, V = least_squares_solver_v2(X_raw, V_raw,
+                                            #   deltaFlat) # original
+                                              scaledDelta) # scaled to reasonable step size
             self.updateMagnitude *= self.updateMagDecay  # Decay the update magnitude
 
             log.debug(f"Norm of a: {np.linalg.norm(a)}")
             update = (V @ a).reshape(-1,1)
+            if (max_update:=np.max(update)) > self.powLimits[1]*0.25:
+                log.warning(f"Large param update detected: {max_update}!")
             scaledUpdate = eta*update
             scaledUpdate = scaledUpdate.flatten()
             log.debug(f"Norm of scaled update: {np.linalg.norm(scaledUpdate)}")
@@ -424,6 +536,3 @@ class HardMZI_v3(MZImesh):
             phase shifter voltages.
         '''
         return {pin_name: volt for pin_name, volt in zip(self.psPins, self.voltages.flatten())}
-
-    # def Update(self, delta: np.ndarray):
-    #     self.records.append(self.stepGradient(delta))

src/vivilux/hardware/daq.py
@@ -72,8 +72,13 @@ class PIN:
         # log.debug(f"(ABSTRACT PIN) Setting pin {self.net_name} to {voltage}V on board {self.board.board_name}")
         return
     
-    def vin(self) -> float:
+    def vin(self, std=False) -> float | tuple[float, float]:
         '''Reads the voltage from the analog input pin.
+        
+        Parameters
+        ----------
+        std : bool, optional
+            Whether to return the standard deviation of the reading (default is False).
         
         Returns
         -------
@@ -280,13 +285,15 @@ class Board:
         #           f" {pin_name} on board {self.board_name}")
         return pin.scan_vin(num_samples)
 
-    def group_vin(self, pin_names: list[str]) -> np.ndarray:
+    def group_vin(self, pin_names: list[str], std: bool = False) -> np.ndarray | tuple[np.ndarray, np.ndarray]:
         '''Reads multiple samples from the specified pins.
         
         Parameters
         ----------
         pin_names : list[str]
             A list of pin names to read from.
+        std : bool, optional
+            Whether to return the standard deviation of the readings (default is False).
         
         Returns
         -------
@@ -550,3 +557,30 @@ class Netlist:
         
         # log.debug(f"(NETLIST) Group scanning {num_samples} samples from pins {net_names}")
         return self.board_dict[board_name].group_scan_vin(net_names, num_samples)
+    
+    def share_board(self, net_names: list[str]) -> Board | None:
+        '''Checks if all the given net names belong to the same board.
+        
+        Parameters
+        ----------
+        net_names : list[str]
+            A list of net names to check.
+        
+        Returns
+        -------
+        Board | None
+            The board if all net names belong to the same board, None otherwise.
+        '''
+        common_board = None
+        for net_name in net_names:
+            if net_name not in self.pins_dict:
+                log.error(f"Net name {net_name} not found in netlist")
+                raise KeyError(f"Net name {net_name} not found in netlist")
+            
+            if common_board is None:
+                common_board = self.pins_dict[net_name].board
+            else:
+                if self.pins_dict[net_name].board is not common_board:
+                    return None # if any net belongs to a different board, return None
+        
+        return common_board

src/vivilux/hardware/detectors.py
@@ -23,6 +23,23 @@ class DetectorArray:
         self.nets = nets
         self.netlist = netlist
         self.transimpedance = transimpedance
+        
+        shared_board = netlist.share_board(nets)
+        if shared_board is None:
+            def read_values(ret_std=False):
+                values = []
+                for net in self.nets:
+                    value = self.netlist[net].vin(std=ret_std)
+                    values.append(value)
+                if ret_std:
+                    vals, stds = zip(*values)
+                    return np.array(vals), np.array(stds)
+                    
+            self._read_values = lambda ret_std=False: read_values(ret_std)
+        else:
+            shared_board: daq.Board
+            self._read_values = lambda ret_std=False: \
+                shared_board.group_vin(self.nets, std=ret_std)
 
         self.read() # Initialize the offsets by reading the detectors without input
     
@@ -33,9 +50,7 @@ class DetectorArray:
 
             TODO: add support for reading multiple pins at once
         '''
-        values = np.zeros(self.size)
-        for i, net in enumerate(self.nets):
-            values[i] = self.netlist[net].vin()
+        values: np.ndarray = self._read_values()
 
         # TODO: refactor to get rid of the if statement (separate offset initialization)
         if not hasattr(self, "offsets"):
@@ -51,3 +66,10 @@ class DetectorArray:
         # NOTE: most c-band detectors have around 0.9 A/W so photocurrent is
         # pretty close to power in Watts
         return np.maximum(reading, 0)
+    
+    def read_raw(self) -> tuple[np.ndarray, np.ndarray]:
+        '''Reads the raw voltage values from the detector nets without offset
+            subtraction or transimpedance conversion.
+        '''
+        values, std_dev = self._read_values(True)
+        return values, std_dev

src/vivilux/hardware/lasers.py
@@ -589,6 +589,8 @@ class SFPDetectorArray(DetectorArray):
         if not isinstance(lasers, SFPLaserArray):
             raise TypeError("lasers must be an instance of SFPLaserArray")
         self.lasers = lasers
+        
+        self.transimpedance = detectors.transimpedance
 
     def read(self):
         '''In this subclass, the reading needs to substract the minimum power
@@ -596,12 +598,12 @@ class SFPDetectorArray(DetectorArray):
         '''
         on_reading = self.detectorArray.read()  # Call the base class read method
         log.info(f"Detector reading with lasers on (mA): {on_reading*1e6}")
-        
-        # turn the lasers to their minimum power
+
+        # if using vibrations, turn the lasers to their minimum power
         if self.lasers.use_vibrations:
             laser_states = self.lasers.board.vibration_states.copy()
         else:
-            laser_states = self.lasers.board.laser_states.copy()
+            return on_reading
         
         self.lasers.setNormalized([0]*self.size)  # Turn off all lasers
         off_reading = self.detectorArray.read()  # Read the detectors with lasers off
@@ -610,6 +612,24 @@ class SFPDetectorArray(DetectorArray):
         self.lasers.setNormalized(laser_states)  # Restore previous laser states
 
         return on_reading - off_reading
+    
+    def read_raw(self):
+        on_reading, on_dev = self.detectorArray.read_raw()  # Call the base class read method
+        log.info(f"Detector reading with lasers on (mA): {on_reading*1e6}")
+        
+        # turn the lasers to their minimum power
+        if self.lasers.use_vibrations:
+            laser_states = self.lasers.board.vibration_states.copy()
+        else:
+            return on_reading, on_dev
+        
+        self.lasers.setNormalized([0]*self.size)  # Turn off all lasers
+        off_reading, off_dev = self.detectorArray.read_raw()  # Read the detectors with lasers off
+        log.info(f"Detector reading with lasers off (mA): {off_reading*1e6}")
+
+        self.lasers.setNormalized(laser_states)  # Restore previous laser states
+
+        return on_reading - off_reading, np.max([on_dev, off_dev], axis=0)
         
 
 if __name__ == "__main__":

src/vivilux/hardware/nidaq.py
@@ -15,11 +15,23 @@ import vivilux.hardware.daq as daq
 class Board(daq.Board):
     '''Base class for National Instruments DAQ boards.
     '''
-    def __init__(self, name: str, dev_serial_num: int, *pins: list[daq.PIN]):
+    def __init__(self,
+                 name: str,
+                 dev_serial_num: int,
+                 *pins: list[daq.PIN],
+                 min_val: float = -0.0,
+                 max_val: float = 2.0,
+                 num_samples: int = 100,
+                 num_samples_to_skip: int = 10,
+                 ):
         super().__init__(name, *pins)
         self.unique_id = dev_serial_num
+        self.min_val = min_val
+        self.max_val = max_val
+        self.num_samples = num_samples
+        self.num_samples_to_skip = num_samples_to_skip
         
-    def group_vin(self, pin_names):
+    def group_vin(self, pin_names, std=False):
         '''Groups the voltage inputs from the specified pins into a single array.
         
         Parameters
@@ -38,11 +50,42 @@ class Board(daq.Board):
                 if not isinstance(pin, AIPIN):
                     raise TypeError(f"Pin {pin_name} is not an AIPIN.")
                 task.ai_channels.add_ai_voltage_chan(f"{self.board_num}/ai{pin.chnl}",
-                                                     min_val=-0.0,
-                                                     max_val=2.0,
+                                                     min_val=self.min_val,
+                                                     max_val=self.max_val,
                                                      terminal_config=nidaqmx.constants.TerminalConfiguration.RSE)
-            data = np.array(task.read(number_of_samples_per_channel=100))
-            data = np.mean(data[:, 10:], axis=1)
+            raw_data = np.array(task.read(number_of_samples_per_channel=self.num_samples))
+            data = np.mean(raw_data[:, self.num_samples_to_skip:], axis=1) # skips first few samples to avoid noise during a transition
+        
+        if std:
+            return data, np.std(raw_data, axis=1)
+        return data
+    
+    def group_scan_vin(self, pin_names, num_samples = 100):
+        '''Groups the voltage inputs from the specified pins into a single array
+            by scanning multiple samples.
+
+        Parameters
+        ----------
+        pin_names : list[str]
+            A list of pin names to read the voltage inputs from.
+        num_samples : int
+            The number of samples to read from each pin.
+
+        Returns
+        -------
+        np.ndarray
+            An array containing the voltage inputs from the specified pins.
+        '''
+        with nidaqmx.Task() as task:
+            for pin_name in pin_names:
+                pin = self.pins[pin_name]
+                if not isinstance(pin, AIPIN):
+                    raise TypeError(f"Pin {pin_name} is not an AIPIN.")
+                task.ai_channels.add_ai_voltage_chan(f"{self.board_num}/ai{pin.chnl}",
+                                                     min_val=self.min_val,
+                                                     max_val=self.max_val,
+                                                     terminal_config=nidaqmx.constants.TerminalConfiguration.RSE)
+            data = np.array(task.read(number_of_samples_per_channel=self.num_samples))
         return data
 
 def get_driver_version() -> namedtuple:
@@ -121,6 +164,7 @@ class USB_6210(Board):
     def __init__(self, name: str, dev_serial_num: int, *pins: list[daq.PIN]):
         super().__init__(name, dev_serial_num, *pins)
 
+# TODO: implement std deviation option for vin like group_vin above
 class AIPIN(daq.PIN):
     direction = daq.PIN_DIRECTION.INPUT
     type = daq.PIN_TYPE.ANALOG